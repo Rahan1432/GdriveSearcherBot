88c72bcf05ab633dd099ce14c609a2626b225c63
Rahan1432 <119683291+Rahan1432@users.noreply.github.com>
2023-01-02 10:34:39 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
-BOT_TOKEN = "1629027959:AAEaTw4s2qaAL3mYP3fQRnE"
+BOT_TOKEN = "5538307814:AAH76zgcR7blYyKYabbOO3wo0PkTZyBi9vY"
 RESULTS_COUNT = 4  # NOTE Number of results to show, 4 is better
-SUDO_CHATS_ID = [-1001485393652, -1005456463651]
+SUDO_CHATS_ID = [-1001707318466]
 
 
 DRIVE_NAME = [
-    "Root",  # folder 1 name
+    "data",  # folder 1 name
     "Cartoon",  # folder 2 name
     "Course",  # folder 3 name
     "Movies",  # ....
     "Series",  # ......
-    "Others"  # and soo onnnn folder n names
+"Others"  # and soo onnnn folder n names
 ]
 
 DRIVE_ID = [
-    "1B9A3QqQqF31IuW2om3Qhr-wkiVLloxw8",  # folder 1 id
-    "12wNJTjNnR-CNBOTnLHqe-1vqFvCRLecn",  # folder 2 id
+    "1-EoC-xfJ5fhAEhEhFw5pDH791TfnGEck",  # folder 1 id
+    "1-EoC-xfJ5fhAEhEhFw5pDH791TfnGEck",  # folder 2 id
     "11nZcObsJJHojHYg43dBS0_eVvJrSD7Nf",  # and so onn... folder n id
     "10_hTMK8HE8k144wOTth_3x1hC2kZL-LR",
     "1-oTctBpyFcydDNiptLL09Enwte0dClCq",
@@ -22,7 +22,7 @@ DRIVE_ID = [
 ]
 
 INDEX_URL = [
-    "https://dl.null.tech/0:",  # folder 1 index link
+    "https://cdn.newarmysquad.workers.dev/0:/data",  # folder 1 index link
     "https://dl.null.tech/0:/Cartoon",  # folder 2 index link
     "https://dl.null.tech/0:/Course",  # and soo on folder n link
     "https://dl.null.tech/0:/MOVIES",
